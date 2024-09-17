725a843eecd5196e386e870d980618ff8e6a9967
blessing-olorunfemi <bolorunfemi1@nait.ca>
2024-09-16 22:55:44 -0600

    commit 3a

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -22,10 +22,10 @@ def lab2Question3(str1, str2):
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
     
-    str1 =str1.count(str2)
-    print(str1)
-    print(str2)
-    return None
+    str1_count = str1.count(str2)
+
+    print(str1_count)
+    return str1_count
     pass
 
 def lab2Question4(list1, list2):
